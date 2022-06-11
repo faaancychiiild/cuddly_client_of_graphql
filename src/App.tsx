2cecbcf891a4068d577bd6aea360eea3c0710851
faaancychiiild <nino.mirziashvili7@gmail.com>
2022-06-11 21:32:22 +0400

    update: routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
-import './App.css'
-import Header from './components/common/header'
-import Technologies from './components/technologies';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import "./App.css"
+import Header from "./components/common/header"
+import Technologies from "./components/technologies"
+import Work from "./components/work"
+
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 
 function App() {
-
   return (
     <>
       <Header />
       <Router>
         <Switch>
-          <Route exact path='/tech' component={Technologies}/>
+          <Route exact path="/tech" component={Technologies} />
+          <Route exact path="/work" component={Work} />
         </Switch>
       </Router>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App'
 import {
   ApolloClient,
   NormalizedCacheObject,
   InMemoryCache,
-  ApolloProvider
-} from '@apollo/client';
+  ApolloProvider,
+} from '@apollo/client'
 
 const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
   cache: new InMemoryCache(),
   uri: 'http://localhost:4000/',
-});
+})
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
 root.render(
   <React.StrictMode>
@@ -24,4 +22,4 @@ root.render(
       <App />
     </ApolloProvider>
   </React.StrictMode>
-);
+)
