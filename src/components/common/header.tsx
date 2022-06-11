6c2330f0226852c2b346bda677b00cd132b58200
faaancychiiild <nino.mirziashvili7@gmail.com>
2022-06-11 21:32:22 +0400

    feat: init nav bar

---
+++ src/components/common/header.tsx
@@ -0,0 +1,19 @@
+import './header.css'
+
+const Header = () => {
+  return (
+    <main className="header">
+      <div className='menu-item'>
+        technologies
+      </div>
+      <div className='menu-item'>
+        work
+      </div>
+      <div className='menu-item'>
+        about
+      </div>
+    </main>
+  )
+}
+
+export default Header
